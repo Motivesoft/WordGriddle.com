1ecd883b2c06c59596643173f3524bb6330f8ef0
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2025-02-16 13:27:31 +0100

    Go a little bit purple

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ themeToggleBtn.addEventListener('click', function () {
     const currentTheme = document.documentElement.getAttribute('data-theme');
     if (currentTheme === 'dark') {
         document.documentElement.removeAttribute('data-theme');
-        themeToggleBtn.textContent = 'Toggle Dark Theme';
+        themeToggleBtn.textContent = 'Dark';
     } else {
         document.documentElement.setAttribute('data-theme', 'dark');
-        themeToggleBtn.textContent = 'Toggle Light Theme';
+        themeToggleBtn.textContent = 'Light';
     }
 });
